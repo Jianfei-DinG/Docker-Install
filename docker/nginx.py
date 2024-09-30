43e8b5680468b0ee23e47189993e2a64f112fb20
JianFei <96579381+Jianfei-DinG@users.noreply.github.com>
2024-10-01 00:18:01 +0800

    Update nginx.py

--- docker/nginx.py
+++ docker/nginx.py
@@ -5,34 +5,25 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-# 定义高亮颜色
-class LogFormatter(logging.Formatter):
-    RESET = "\033[0m"
-    INFO_COLOR = "\033[94m"  # 蓝色
-    ERROR_COLOR = "\033[91m"  # 红色
-
-    def format(self, record):
-        if record.levelno == logging.INFO:
-            record.msg = f"{self.INFO_COLOR}{record.msg}{self.RESET}"
-        elif record.levelno == logging.ERROR:
-            record.msg = f"{self.ERROR_COLOR}{record.msg}{self.RESET}"
-        return super().format(record)
-
 # 配置日志
-formatter = LogFormatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-for handler in logging.getLogger().handlers:
-    handler.setFormatter(formatter)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 class NginxEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.last_reload_time = 0
+        self.reload_interval = 5  # 设置重载间隔为5秒
+
     def on_modified(self, event):
         if event.src_path.endswith('.conf'):
-            logging.info(f'检测到 {event.src_path} 的变化，正在重载 Nginx...')
-            result = subprocess.run(['nginx', '-s', 'reload'])
-            if result.returncode == 0:
-                logging.info('Nginx 重载成功。')
-            else:
-                logging.error('Nginx 重载失败。')
+            current_time = time.time()
+            if current_time - self.last_reload_time > self.reload_interval:
+                logging.info(f'检测到 {event.src_path} 的变化，正在重载 Nginx...')
+                result = subprocess.run(['nginx', '-s', 'reload'])
+                if result.returncode == 0:
+                    logging.info('Nginx 重载成功。')
+                    self.last_reload_time = current_time
+                else:
+                    logging.error('Nginx 重载失败。')
 
     def on_created(self, event):
         logging.info(f'创建了配置文件: {event.src_path}')
@@ -43,7 +34,6 @@ class NginxEventHandler(FileSystemEventHandler):
         self.on_modified(event)
 
 def check_nginx_config():
-    # 检查 Nginx 配置文件的有效性
     result = subprocess.run(['nginx', '-t'], capture_output=True, text=True)
     if result.returncode != 0:
         logging.error("Nginx 配置检查失败：")
@@ -52,14 +42,12 @@ def check_nginx_config():
     logging.info("Nginx 配置检查通过。")
 
 def start_nginx():
-    # 启动 Nginx
     subprocess.Popen(['nginx', '-g', 'daemon off;'])
 
 if __name__ == "__main__":
     check_nginx_config()  # 检查配置
     start_nginx()         # 启动 Nginx
 
-    # 监控的目录
     paths = ["/etc/nginx/http.d", "/etc/nginx/stream.d"]
     event_handler = NginxEventHandler()
     observer = Observer()
